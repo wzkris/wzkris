6aa70e46597a678a855fc321a25d33ccadd74c54
wzkris <1439804473@qq.com>
2024-12-10 10:09:32 +0800

    扩展MybatisPlus方法

---
+++ wzkris-common/wzkris-common-orm/src/main/java/com/wzkris/common/orm/plus/extension/ExtenseSqlInjector.java
@@ -0,0 +1,19 @@
+package com.wzkris.common.orm.plus.extension;
+
+import com.baomidou.mybatisplus.core.injector.AbstractMethod;
+import com.baomidou.mybatisplus.core.injector.DefaultSqlInjector;
+import com.baomidou.mybatisplus.core.metadata.TableInfo;
+import com.wzkris.common.orm.plus.extension.method.SelectByIdForUpdate;
+import org.apache.ibatis.session.Configuration;
+
+import java.util.List;
+
+public class ExtenseSqlInjector extends DefaultSqlInjector {
+
+    @Override
+    public List<AbstractMethod> getMethodList(Configuration configuration, Class<?> mapperClass, TableInfo tableInfo) {
+        List<AbstractMethod> methodList = super.getMethodList(configuration, mapperClass, tableInfo);
+        methodList.add(new SelectByIdForUpdate());
+        return methodList;
+    }
+}
