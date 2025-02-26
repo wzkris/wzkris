c5cd31a98f8da8fb1344dc833abdb2dcec7db993
wzkris <1439804473@qq.com>
2025-02-26 16:57:19 +0800

    优化验证码资源加载逻辑 支持OAuth2AccessToken序列化

--- wzkris-common/wzkris-common-security/src/main/java/com/wzkris/common/security/serialize/OAuth2CustomModule.java
+++ wzkris-common/wzkris-common-security/src/main/java/com/wzkris/common/security/serialize/OAuth2CustomModule.java
@@ -1,8 +1,10 @@
 package com.wzkris.common.security.serialize;
 
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.wzkris.common.security.deserialize.OAuth2AccessTokenTypeDeserializer;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.server.authorization.authentication.OAuth2ClientAuthenticationToken;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
@@ -13,10 +15,13 @@ public class OAuth2CustomModule extends SimpleModule {
 
     public OAuth2CustomModule() {
         super(OAuth2CustomModule.class.getName());
+        this.addDeserializer(OAuth2AccessToken.TokenType.class, new OAuth2AccessTokenTypeDeserializer());
     }
 
     @Override
     public void setupModule(SetupContext context) {
+        super.setupModule(context);
+        context.setMixInAnnotations(OAuth2AccessToken.class, OAuth2AccessTokenMixin.class);
         context.setMixInAnnotations(BearerTokenAuthentication.class, BearerTokenAuthenticationMixin.class);
         context.setMixInAnnotations(OAuth2ClientAuthenticationToken.class, OAuth2ClientAuthenticationTokenMixin.class);
         context.setMixInAnnotations(ClientAuthenticationMethod.class, ClientAuthenticationMethodMixin.class);
