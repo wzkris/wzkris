75aa6af0783b435cc6ed154d74988f103b73aadc
wzkris <1439804473@qq.com>
2025-03-21 08:40:21 +0800

    移除设备模块

--- wzkris-modules/wzkris-modules-equipment/wzkris-equipment-biz/src/main/java/com/wzkris/equipment/constants/DeviceConstant.java
+++
@@ -1,7 +0,0 @@
-package com.wzkris.equipment.constants;
-
-public interface DeviceConstant {
-
-    String ROOM_PREFIX = "device_rooms:";
-
-}
--- wzkris-modules/wzkris-modules-equipment/wzkris-equipment-biz/src/main/java/com/wzkris/equipment/service/ThingsModelService.java
+++
@@ -1,6 +0,0 @@
-package com.wzkris.equipment.service;
-
-public interface ThingsModelService {
-
-    boolean insertModel();
-}
