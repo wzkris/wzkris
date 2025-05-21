2e042faaff301cb2ea860a46eff0f57dc00a7772
wzkris <1439804473@qq.com>
2025-05-21 16:43:06 +0800

    chore: 替换魔法值

--- wzkris-common/wzkris-common-security/src/main/java/com/wzkris/common/security/oauth2/resolver/CustomBearerTokenResolver.java
+++ wzkris-common/wzkris-common-security/src/main/java/com/wzkris/common/security/oauth2/resolver/CustomBearerTokenResolver.java
@@ -3,6 +3,7 @@ package com.wzkris.common.security.oauth2.resolver;
 import com.wzkris.common.core.utils.StringUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpHeaders;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.server.resource.web.BearerTokenResolver;
 import org.springframework.security.oauth2.server.resource.web.DefaultBearerTokenResolver;
 
@@ -23,8 +24,8 @@ public class CustomBearerTokenResolver implements BearerTokenResolver {
     @Override
     public String resolve(HttpServletRequest request) {
         String token = StringUtil.blankToDefault(defaultBearerTokenResolver.resolve(request), request.getHeader(HttpHeaders.AUTHORIZATION));
-        if (StringUtil.isNotBlank(token) && token.startsWith("Bearer ")) {
-            token = token.replaceFirst("Bearer ", "");
+        if (StringUtil.isNotBlank(token) && token.startsWith(OAuth2AccessToken.TokenType.BEARER.getValue() + StringUtil.SPACE)) {
+            token = token.replaceFirst(OAuth2AccessToken.TokenType.BEARER.getValue() + StringUtil.SPACE, StringUtil.EMPTY);
         }
         return token;
     }
