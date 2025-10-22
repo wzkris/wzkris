400edfaa40d3cfb678b9416b88a81208ecd4ef51
wzkris <1439804473@qq.com>
2025-10-22 13:45:11 +0800

    fix: 标记为springbean

--- wzkris-common/wzkris-common-security/src/main/java/com/wzkris/common/security/config/SecurityAutoConfiguration.java
+++ wzkris-common/wzkris-common-security/src/main/java/com/wzkris/common/security/config/SecurityAutoConfiguration.java
@@ -7,12 +7,13 @@ import com.wzkris.common.security.oauth2.ResourceServerConfig;
 import com.wzkris.common.security.oauth2.filter.RequestSignatureFilter;
 import com.wzkris.common.security.utils.LoginClientUtil;
 import com.wzkris.common.security.utils.LoginCustomerUtil;
+import com.wzkris.common.security.utils.LoginStaffUtil;
 import com.wzkris.common.security.utils.LoginUserUtil;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Import;
 
 @Import({ResourceServerConfig.class, RequestSignatureFilter.class,
-        LoginUserUtil.class, LoginCustomerUtil.class, LoginClientUtil.class,
+        LoginUserUtil.class, LoginStaffUtil.class, LoginCustomerUtil.class, LoginClientUtil.class,
         CheckPermsAspect.class, FieldPermsAspect.class, SecurityExceptionHandler.class})
 @AutoConfiguration
 public class SecurityAutoConfiguration {
