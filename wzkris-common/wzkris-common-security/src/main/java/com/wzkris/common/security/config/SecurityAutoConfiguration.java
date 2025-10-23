b37c61bb4f976ef9bcb8622eb8b2ada502b675e9
wzkris <1439804473@qq.com>
2025-10-23 14:52:17 +0800

    remove：移除字段权限，该设计不太好用

--- wzkris-common/wzkris-common-security/src/main/java/com/wzkris/common/security/config/SecurityAutoConfiguration.java
+++ wzkris-common/wzkris-common-security/src/main/java/com/wzkris/common/security/config/SecurityAutoConfiguration.java
@@ -1,7 +1,6 @@
 package com.wzkris.common.security.config;
 
 import com.wzkris.common.security.annotation.aspect.CheckPermsAspect;
-import com.wzkris.common.security.annotation.aspect.FieldPermsAspect;
 import com.wzkris.common.security.handler.SecurityExceptionHandler;
 import com.wzkris.common.security.oauth2.ResourceServerConfig;
 import com.wzkris.common.security.oauth2.filter.RequestSignatureFilter;
@@ -14,7 +13,7 @@ import org.springframework.context.annotation.Import;
 
 @Import({ResourceServerConfig.class, RequestSignatureFilter.class,
         LoginUserUtil.class, LoginStaffUtil.class, LoginCustomerUtil.class, LoginClientUtil.class,
-        CheckPermsAspect.class, FieldPermsAspect.class, SecurityExceptionHandler.class})
+        CheckPermsAspect.class, SecurityExceptionHandler.class})
 @AutoConfiguration
 public class SecurityAutoConfiguration {
 
